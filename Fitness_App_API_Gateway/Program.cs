32c54a5783beea26e3c71f1d34dd64550b3fdebc
S0rgi <ilyuxa.chernov.04@mail.ru>
2025-12-07 14:00:36 +0300

    add avatars redirect

--- Fitness_App_API_Gateway/Program.cs
+++ Fitness_App_API_Gateway/Program.cs
@@ -13,6 +13,9 @@ builder.Configuration["ReverseProxy:Clusters:workoutCluster:Destinations:workout
 builder.Configuration["ReverseProxy:Clusters:notificationsCluster:Destinations:notifications:Address"] =
     Environment.GetEnvironmentVariable("NOTIFICATION_API_URL");
 
+builder.Configuration["ReverseProxy:Clusters:avatarsCluster:Destinations:avatars:Address"] =
+    Environment.GetEnvironmentVariable("AVATARS_API_URL");
+
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
